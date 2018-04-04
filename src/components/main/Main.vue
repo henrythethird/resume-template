7a29e79a280974d4b5af92be8d291e7a54039b4f
Patrick <patrick.kaufmann@pwc-digital.ch>
2018-04-04 16:04:44 +0200

    Content

---
+++ src/components/main/Main.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main">
+    <h1 class="name">{{ name }}</h1>
+    <h2 class="position">Software Engineering Student</h2>
+
+    <Section><template slot="title">work experience</template></Section>
+  </div>
+</template>
+
+<script>
+import Section from './Section'
+
+export default {
+  name: 'Main',
+  props: ['name'],
+  components: {
+    Section
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .name {
+    padding: 0;
+    margin-top: 20px;
+    font-size: 50px;
+  }
+
+  .position {
+    font-size: 16px;
+    font-style: italic;
+    opacity: 0.6;
+    letter-spacing: 3px;
+    font-variant: small-caps;
+    margin-bottom: 124px;
+  }
+
+  .main {
+    padding: 50px;
+  }
+</style>
---
+++ src/components/side/InfoTable.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="info-table">
+    <Title><slot name="title">Replace me</slot></Title>
+
+    <div class="infos">
+      <div v-for="info in infos" :key="info.name" class="info">
+        <div class="icon">{{ info.icon }}</div>
+
+        <div class="description">
+          <div class="name">{{ info.title }}</div>
+          <div class="subtitle">{{ info.subtitle }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from './Title'
+
+export default {
+  name: 'InfoTable',
+  props: ['infos'],
+  components: {
+    Title
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .info-table {
+    color: white;
+    margin: 0 0 20px 50px;
+  }
+
+  .infos {
+    margin-top: 15px;
+  }
+
+  .info {
+    display: flex;
+    padding: 5px 0;
+  }
+
+  .icon {
+    flex: 0 0 40px;
+    margin-right: 10px;
+  }
+
+  .name {
+    font-size: 14px;
+  }
+
+  .subtitle {
+    font-size: 13px;
+    opacity: 0.6;
+    font-style: italic;
+    white-space: pre-wrap;
+  }
+</style>
+
